e9ac6b7ac7d1d9d8881341b9245f1ffd79db9071
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-09-21 23:48:38 +0200

    finish

--- src/components/Api/PixabayApi.jsx
+++ src/components/Api/PixabayApi.jsx
@@ -3,9 +3,9 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const yourKey = '38650686-211065fda926dbe73f41f5be1'
 
-export const FetchPixabay = async (search, page) => {
+export const FetchPixabay = async (searchQuery, page) => {
     const { data } = await axios
-        .get(`https://pixabay.com/api/?q=${search}&page=${page}&key=${yourKey}&image_type=photo&orientation=horizontal&per_page=12`);
-    return data
+        .get(`https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${yourKey}&image_type=photo&orientation=horizontal&per_page=12`);
+    return data;
 }
 
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,45 +1,48 @@
 
 import React, { Component } from 'react'
 import { ImSearch } from 'react-icons/im'
-import { toast } from 'react-toastify'
+// import { toast } from 'react-toastify'
+import Notiflix from 'notiflix';
+import './Searchbar.css'
 export class Searchbar extends Component {
     state = {
-        search: '',
+        searchQuery: '',
     }
     handleChange = event => {
-        this.setState({ search: event.currentTarget.value.toLowerCase() })
+        this.setState({ searchQuery: event.currentTarget.value.toLowerCase() })
     }
 
     handleSubmit = event => {
         event.preventDefault()
-        if (this.state.search.trim() === '') {
-            toast.error('Please enter your search query!🙏')
+        if (this.state.searchQuery.trim() === '') {
+            Notiflix.Notify.failure('Please enter your search query!🙏')
             return;
         }
-        this.props.onSubmit(this.state.search);
-        this.setState({ search: '' })
+        this.props.onSubmit(this.state.searchQuery);
+        this.setState({ searchQuery: '' })
     }
 
     render() {
+
         return (
-            <header onSubmit={this.handleSubmit} class="searchbar">
-                <form class="form">
+            <header onSubmit={this.handleSubmit} className="searchbar">
+                <form className="form">
                     <button
                         type="submit"
-                        class="button"
+                        className="Search-button"
                     >
                         <ImSearch style={{ marginRight: 8 }} />
-                        <span class="button-label">Search</span>
+                        <span className="button-label">Search</span>
                     </button>
 
                     <input
                         name='search'
-                        value={this.state.search}
+                        value={this.state.searchQuery}
                         onChange={this.handleChange}
-                        class="input"
+                        className="input"
                         type="text"
-                        autocomplete="off"
-                        autofocus
+                        autoComplete="off"
+                        autoFocus
                         placeholder="Search images and photos"
                     />
                 </form>
